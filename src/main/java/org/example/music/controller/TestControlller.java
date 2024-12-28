3f815a3160a4b8784ab50fa397880f0f205575b1
27542 <2754216911@qq.com>
2024-12-28 08:27:21 +0800

    The forth day

--- src/main/java/org/example/music/controller/TestControlller.java
+++ src/main/java/org/example/music/controller/TestControlller.java
@@ -1,14 +1,11 @@
 package org.example.music.controller;
 
-import org.example.music.domain.Admin;
 import org.example.music.domain.Singer;
-import org.example.music.domain.Test;
 import org.example.music.mapper.SingerMapper;
 import org.example.music.service.SingerService;
 import org.example.music.service.TestService;
 import org.example.music.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
