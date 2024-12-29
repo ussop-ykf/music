6fd7a08447193391bfe677284a4e3de0174c2f87
27542 <2754216911@qq.com>
2024-12-29 08:50:03 +0800

    1

---
+++ src/pages/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="login-wrap">
+    <div class="ms-title">music 后台管理登录</div>
+    <div class="ms-login">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item prop="username">
+          <el-input v-model="ruleForm.username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="ruleForm.password" placeholder="密码"></el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm">登录</el-button>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mixin} from '../mixins/index'
+import {checkAdmin} from '../api'
+
+export default {
+  mixins: [mixin],
+  data: function () {
+    return {
+      ruleForm: {
+        username: '', password: ''
+      },
+      // 验证规则
+      rules: {
+        username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+        password: [{required: true, message: '请输入密码', trigger: 'blur'}, {min: 6, max: 10, message: '密码长度6-10位'}]
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      this.$refs['ruleForm'].validate((valid) => {
+        if (valid) {
+          // true: 发送请求到后台接口
+          checkAdmin('/api/system/admin/login', this.ruleForm).then(res => {
+            if (res.data === 1) {
+              this.$message.success(res.msg)
+            } else {
+              this.$message.error(res.msg)
+            }
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.login-wrap {
+  position: relative;
+  background: url("../assets/img/background.jpg") fixed center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.ms-title {
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  margin-top: -230px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #fff;
+}
+
+.ms-login {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 300px;
+  height: 160px;
+  margin-left: -190px;
+  margin-top: -150px;
+  padding: 40px;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.login-btn {
+  text-align: center;
+}
+
+.login-btn button {
+  width: 100%;
+  height: 36px;
+}
+</style>
---
+++ src/pages/SongListPage.vue
@@ -0,0 +1,201 @@
+<template>
+    <div class="table">
+        <div class="container">
+            <div class="handle-box">
+                <el-button type="primary" size="mini" @click="delAll">批量删除</el-button>
+                <el-input v-model="select_word" size="mini" placeholder="筛选关键字" class="handle-input"></el-input>
+                <el-button type="primary" size="mini" @click="centerDialogVisible = true">添加歌单</el-button>
+            </div>
+        </div>
+        <el-table size="mini" ref="multipleTable" border style="width:100%" height="539px" :data="data" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="40"></el-table-column>
+            <el-table-column label="歌单图片" width="110" align="center">
+                <template slot-scope="scope">
+                    <div class="songList-img">
+                        <img :src="getUrl(scope.row.pic)" style="width:100%"/>
+                    </div>
+                    <el-upload :action="uploadUrl(scope.row.id)" :before-upload="beforeAvatorUpload"
+                        :on-success="handleAvatorSuccess">
+                        <el-button size="mini">更新图片</el-button>
+                    </el-upload>
+                </template>
+            </el-table-column>
+            <el-table-column prop="title" label="标题" width="120" align="center"></el-table-column>
+            <el-table-column label="简介">
+                <template slot-scope="scope">
+                    <p style="height:100px;overflow:scroll">{{scope.row.introduction}}</p>
+                </template>
+            </el-table-column>
+            <el-table-column prop="style" label="风格" width="120" align="center"></el-table-column>
+            <el-table-column label="歌曲管理" width="110" align="center">
+                <template slot-scope="scope">
+                    <el-button size="mini" @click="songEdit(scope.row.id)">歌曲管理</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="评论" width="80" align="center">
+                <template slot-scope="scope">
+                    <el-button size="mini" @click="getComment(data[scope.$index].id)">评论</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="150" align="center">
+                <template slot-scope="scope">
+                    <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+                    <el-button size="mini" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div class="pagination">
+            <el-pagination
+                background
+                layout = "total,prev,pager,next"
+                :current-page="currentPage"
+                :page-size="pageSize"
+                :total="tableData.length"
+                @current-change="handleCurrentChange"
+                >
+            </el-pagination>
+        </div>
+
+        <el-dialog title="添加歌单" :visible.sync="centerDialogVisible" width="400px" center>
+            <el-form :model="registerForm" ref="registerForm" label-width="80px">
+                <el-form-item prop="title" label="标题" size="mini">
+                    <el-input v-model="registerForm.title" placeholder="标题"></el-input>
+                </el-form-item>
+                <el-form-item prop="introduction" label="简介" size="mini">
+                    <el-input v-model="registerForm.introduction" placeholder="简介" type="textarea"></el-input>
+                </el-form-item>
+                <el-form-item prop="style" label="风格" size="mini">
+                    <el-input v-model="registerForm.style" placeholder="风格"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer">
+                <el-button size="mini" @click="centerDialogVisible = false">取消</el-button>
+                <el-button size="mini" @click="addSongList">确定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog title="修改歌单" :visible.sync="editVisible" width="400px" center>
+            <el-form :model="form" ref="form" label-width="80px">
+                <el-form-item prop="title" label="标题" size="mini">
+                    <el-input v-model="form.title" placeholder="标题"></el-input>
+                </el-form-item>
+                <el-form-item prop="introduction" label="简介" size="mini">
+                    <el-input v-model="form.introduction" placeholder="简介" type="textarea"></el-input>
+                </el-form-item>
+                <el-form-item prop="style" label="风格" size="mini">
+                    <el-input v-model="form.style" placeholder="风格"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer">
+                <el-button size="mini" @click="editVisible = false">取消</el-button>
+                <el-button size="mini" @click="editSave">确定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog title="删除歌单" :visible.sync="delVisible" width="300px" center>
+            <div align="center">删除不可恢复，是否确定删除？</div>
+            <span slot="footer">
+                <el-button size="mini" @click="delVisible = false">取消</el-button>
+                <el-button size="mini" @click="deleteRow">确定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+import {getAllSongList, setSongList, updateSongList, delSongList} from '../api/index'
+import { mixin } from '../mixins/index'
+export default {
+  mixins: [mixin],
+  data () {
+    return {
+      centerDialogVisible: false, // 添加弹窗是否显示
+      editVisible: false, // 编辑弹窗是否显示
+      delVisible: false, // 删除弹窗是否显示
+      registerForm: { // 添加框
+        title: '',
+        introduction: '',
+        style: ''
+      },
+      form: { // 编辑框
+        id: '',
+        title: '',
+        introduction: '',
+        style: ''
+      },
+      tableData: [],
+      tempData: [],
+      select_word: '',
+      pageSize: 4, // 分页每页大小
+      currentPage: 1, // 当前页
+      idx: -1, // 当前选择项
+      multipleSelection: [] // 哪些项已经打勾
+    }
+  },
+  computed: {
+    // 计算当前搜索结果表里的数据
+    data () {
+      return this.tableData.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize)
+    }
+  },
+  watch: {
+    // 搜索框里面的内容发生变化的时候，搜索结果table列表的内容跟着它的内容发生变化
+    select_word: function () {
+      if (this.select_word === '') {
+        this.tableData = this.tempData
+      } else {
+        this.tableData = []
+        for (let item of this.tempData) {
+          if (item.title.includes(this.select_word)) {
+            this.tableData.push(item)
+          }
+        }
+      }
+    }
+  },
+  created () {
+
+  },
+  methods: {
+    // 获取当前页
+
+    // 查询所有歌单
+
+    // 添加歌单
+
+    // 弹出编辑页面
+
+    // 保存编辑页面修改的数据
+
+    // 更新图片
+
+    // 删除一个歌单
+
+    // 转向歌曲管理页面
+
+    // 转向该歌单的评论列表
+
+  }
+}
+</script>
+
+<style scoped>
+    .handle-box{
+        margin-bottom: 0px;
+    }
+    .songList-img{
+        width: 100%;
+        height: 80px;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        overflow: hidden;
+    }
+    .handle-input{
+        width: 300px;
+        display: inline-block;
+    }
+    .pagination{
+        display: flex;
+        justify-content: center;
+    }
+</style>
